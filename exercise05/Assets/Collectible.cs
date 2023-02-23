0e08198b21c17d22f4805d0f36a55db54659d612
Sara Bayat Hashemi <s.bayathashemi@yahoo.com>
2023-02-23 16:50:17 -0500

    Script Added

--- exercise05/Assets/Collectible.cs
+++ exercise05/Assets/Collectible.cs
@@ -8,39 +8,42 @@ public class Collectible : MonoBehaviour
 {
     public TMP_Text scoreText;
     public GameObject particleEffect;
-    int score; 
+    public GameObject CanvasObj;
+
+    static int score;
     //public var destroyDelay = 3.0;
 
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;
+        CanvasObj.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Collectible"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            score += 1;
-            SetScoreText(); 
-            Instantiate(particleEffect, other.gameObject.transform.position, other.gameObject.transform.rotation);
-       
-            Destroy(other.gameObject, 0.25f);
-            DestroyParticles();
+            score++;
+            SetScoreText();
+            Instantiate(particleEffect, this.gameObject.transform.position, this.gameObject.transform.rotation);
+
+            Destroy(this.gameObject);
+
+        }
+
+        if (score >= 4)
+        {
+            CanvasObj.SetActive(true);
+            Instantiate(CanvasObj);
         }
     }
 
-    private void DestroyParticles()
-    {
-
-        Destroy(particleEffect);
-    }
 
     void SetScoreText()
     {
